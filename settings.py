963678db37b070b5203404896b315ef5b62e668e
challenger128 <47565385+challenger128@users.noreply.github.com>
2021-07-22 22:21:26 +0500

    Added class for storing game settings, because usage will be simpler

---
+++ settings.py
@@ -0,0 +1,12 @@
+class Setting:
+    """
+    Class for storing game settings
+    """
+    def __init__(self):
+        """
+        Init game settings, just usual for all
+        """
+        self.screen_width = 1270
+        self.screen_height = 720
+        self.bg_color = (230, 230, 230)
+
