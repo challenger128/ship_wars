0f07f2bda4179277491031a161390d9132bd0372
challenger128 <47565385+challenger128@users.noreply.github.com>
2021-07-22 23:20:48 +0500

    Refactored blit method. Also added docs for all in ship.py

--- ship.py
+++ ship.py
@@ -2,14 +2,24 @@ import pygame
 
 
 class Ship:
-
+    """
+    Class for make ships
+    """
     def __init__(self, screen):
+        """
+        Init our ship, loading a sprite and make a rectangle
+        :param screen: requires screen where our ship will be shown
+        """
         self.screen = screen
-        self.image = pygame.image.load('assets\ship.bmp')
+        self.image = pygame.image.load('alien_invasion/assets/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.centery = self.screen_rect.centery
 
-    def blit_ship(self):
+    def blit(self):
+        """
+        Draws ship in its position
+        :return: None
+        """
         self.screen.blit(self.image, self.rect)
