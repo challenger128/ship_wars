0ffe5fe37234c1f4116197ab78cf78e77b77a29f
challenger128 <47565385+challenger128@users.noreply.github.com>
2021-07-22 19:05:28 +0500

    Created main.py which will be a template for any game written in pygame-engine

---
+++ main.py
@@ -0,0 +1,47 @@
+# Import a library of functions called 'pygame'
+import pygame
+from math import pi
+
+# Initialize the game engine
+pygame.init()
+
+# Define the colors we will use in RGB format
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+# Set the height and width of the screen
+size = [400, 300]
+screen = pygame.display.set_mode(size)
+FPS = 120
+
+pygame.display.set_caption("Alien Invasion")
+
+# Loop until the user clicks the close button.
+done = False
+clock = pygame.time.Clock()
+
+while not done:
+
+    # This limits the while loop to a max of 10 times per second.
+    # Leave this out and we will use all CPU we can.
+    clock.tick(FPS)
+
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+
+    # All drawing code happens after the for loop and but
+    # inside the main while done==False loop.
+
+    # Clear the screen and set the screen background
+    screen.fill(WHITE)
+
+    # Go ahead and update the screen with what we've drawn.
+    # This MUST happen after all the other drawing commands.
+    pygame.display.flip()
+
+# Be IDLE friendly
+pygame.quit()
