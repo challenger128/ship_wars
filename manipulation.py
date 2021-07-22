096799c01c44e84bf318b14d6f019cd832fd9481
challenger128 <47565385+challenger128@users.noreply.github.com>
2021-07-23 00:08:45 +0500

    Added continuous ship movement for right and left key

--- manipulation.py
+++ manipulation.py
@@ -12,6 +12,17 @@ def check_events(ship):
         if event.type == pygame.QUIT:  # If user clicked close
             pygame.quit()
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = False
+
 
 def update_screen(game_setting, screen, ship):
     """
