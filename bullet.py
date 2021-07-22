e6904a06cb81658abc949f24b31dec0aefefe6bf
challenger128 <47565385+challenger128@users.noreply.github.com>
2021-07-23 01:08:56 +0500

    Added docs

--- bullet.py
+++ bullet.py
@@ -5,7 +5,13 @@ from pygame.sprite import Sprite
 class Bullet(Sprite):
 
     def __init__(self, screen, game_setting, ship):
-
+        """
+        Init bullet which inherited by Sprite.
+        We will use Group() for make bullet manipulation simpler
+        :param screen: required for output our bullet
+        :param game_setting: required for bullet setting
+        :param ship: required for bullet position
+        """
         super(Bullet, self).__init__()
         self.screen = screen
         self.rect = pygame.Rect(0, 0, game_setting.bullet_width,
@@ -16,7 +22,15 @@ class Bullet(Sprite):
         self.speed = game_setting.bullet_speed
 
     def update(self):
+        """
+        Updates bullet position
+        :return: None
+        """
         self.rect.y -= self.speed
 
     def draw(self):
+        """
+        Draws simple rectangle which is the bullet
+        :return: None
+        """
         pygame.draw.rect(self.screen, self.color, self.rect)
