20474b226a402036d26722266627d70d2de19090
challenger128 <47565385+challenger128@users.noreply.github.com>
2021-07-24 02:12:42 +0500

    Tidied up

--- main.py
+++ main.py
@@ -2,25 +2,21 @@ import pygame
 from pygame.sprite import Group
 from settings import Setting
 from ship import Ship
-from text import Text
 from manipulation import check_events, update_screen
-
 
 pygame.init()
 game_setting = Setting()
 screen = pygame.display.set_mode([game_setting.screen_width,
                                  game_setting.screen_height])
 pygame.display.set_caption(game_setting.name)
+pygame.display.set_icon(pygame.image.load('ship_wars/assets/ship.png'))
 clock = pygame.time.Clock()
 ship = Ship(screen, game_setting)
 bullets = Group()
-bullets_have = Text(screen, game_setting, '')
-
 
 while True:
     clock.tick(game_setting.fps)
     check_events(screen, game_setting, ship, bullets)
     ship.update()
     bullets.update()
-    bullets_have.update('Bullets ' + str(game_setting.bullet_allowed - len(bullets)))
-    update_screen(screen, game_setting, ship, bullets, bullets_have)
+    update_screen(screen, game_setting, ship, bullets)
--- settings.py
+++ settings.py
@@ -6,15 +6,11 @@ class Setting:
         """
         Init game settings, just usual for all
         """
-        self.screen_width = 1270
+        self.screen_width = 1280
         self.screen_height = 720
         self.bg_color = (23, 16, 16)
         self.fps = 120
         self.name = 'Ship Wars. Prototype'
         self.ship_speed = 3
-        self.bullet_speed = 4
-        self.bullet_width = 4
-        self.bullet_height = 12
-        self.bullet_color = (89, 82, 96)
+        self.bullet_speed = 3
         self.bullet_allowed = 4
-        self.text_color = (254, 152, 152)
