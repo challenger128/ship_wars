1f19bda29c3143db509300cceb7552ac942a68ad
challenger128 <47565385+challenger128@users.noreply.github.com>
2021-07-24 03:25:55 +0500

    Tidied up due to other changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Setting
 from ship import Ship
-from manipulation import check_events, update_screen
+from manipulation import check_events, update_screen, create_enemies
 
 pygame.init()
 game_setting = Setting()
@@ -13,10 +13,11 @@ clock = pygame.time.Clock()
 background = pygame.image.load('ship_wars/assets/bg.png').convert_alpha()
 ship = Ship(screen, game_setting)
 bullets = Group()
-
+enemies = Group()
+create_enemies(screen, game_setting, enemies)
 while True:
     clock.tick(game_setting.fps)
     check_events(screen, game_setting, ship, bullets)
     ship.update()
     bullets.update()
-    update_screen(screen, background, game_setting, ship, bullets)
+    update_screen(screen, background, game_setting, ship, enemies, bullets)
